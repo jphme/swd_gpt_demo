4bbda1472fbc242155aebff2ac6f16439bf78cfa
Jan Philipp Harries <jpdus@users.noreply.github.com>
2023-05-11 09:50:26 +0200

    Update main.py
    geduld

--- main.py
+++ main.py
@@ -73,7 +73,7 @@ def text_area_callback(spinner_placeholder):
     st.session_state['chat_text'] += f"**You:**\n {st.session_state.user_input}\n\n"
 
     #with spinner_placeholder.spinner("Die KI generiert eine Antwort, bitte habe etwas Geduld..."):
-    spinner_placeholder.markdown('<i class="fas fa-spinner fa-spin"></i> Die KI generiert eine Antwort...', 
+    spinner_placeholder.markdown('<i class="fas fa-spinner fa-spin"></i> Die KI generiert eine Antwort, bitte habe etwas Geduld...', 
                                  unsafe_allow_html=True)
     ai_response = st.session_state['chain'].predict(input=st.session_state.user_input)
     spinner_placeholder.markdown("", unsafe_allow_html=True)
